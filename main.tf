d95b6550f7e72cda04e2f7f862206139dca0a74d
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-09 15:26:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,37 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "~> 3.27"
     }
   }
-  required_version = ">= 1.1.1"
 
-  cloud {
-    organization = "systemEngineering"
-
-    workspaces {
-      name = "SystemEngineeringNew"
-    }
-  }
+  required_version = ">= 0.14.9"
 }
-
 
 provider "aws" {
-  region = "us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
