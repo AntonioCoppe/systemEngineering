2bc7374a3c8825d9798d3e36cc96132a4ca70e8f
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-09 13:30:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.1"
+  required_version = ">= 1.1.0"
 
   cloud {
     organization = "sciorcoppe"
@@ -22,18 +22,33 @@ terraform {
 
 
 provider "aws" {
-  region = "us-north-1"
+  region = "us-west-2"
 }
+
+
+
+resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "web-address" {
