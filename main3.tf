fc6216ee266a6dea1b1b4e6eee70b403cab03b4a
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-09 15:20:33 +0100

    Create main3.tf

---
+++ main3.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+  }
+  required_version = ">= 1.1.1"
+
+  cloud {
+    organization = "systemEngineering"
+
+    workspaces {
+      name = "SystemEngineeringNew"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
