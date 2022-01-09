59c27a63353eb39268f904602a9f99eabbacf5eb
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-09 13:52:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,25 +9,30 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = ">= 1.1.1"
+  required_version = ">= 1.1.0"
 
   cloud {
-    organization = "sciorcoppe"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "systemsEngineering"
+      name = "gh-actions-demo"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-north-1"
+  region = "us-west-2"
 }
+
+
+
+resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -36,8 +41,16 @@ resource "aws_instance" "web" {
               EOF
 }
 
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
-
-
