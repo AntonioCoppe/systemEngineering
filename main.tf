25f432ef0d72e09ed97a03d6bece28537add67e7
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-09 17:30:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,10 +21,10 @@ module "bootstrap" {
   source                      = "./modules/bootstrap"
   name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
   dynamo_db_table_name        = "aws-locks"
-  iam_user_name               = "GitHubActionsIamUser"
-  ado_iam_role_name           = "GitHubActionsIamRole"
-  aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
-  aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
+  #iam_user_name               = "GitHubActionsIamUser"
+  #ado_iam_role_name           = "GitHubActionsIamRole"
+  #aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
+  #aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
 }
   
 
