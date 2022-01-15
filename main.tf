9f3f659330ad31117524d2b5db7a5634e3676ba4
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-15 17:42:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,10 +39,7 @@ resource "aws_instance" "web" {
               echo "hello world"
               sudo terminate-instance
               echo "terminated haha"
-              sed -i '1s/^/deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse\n
-              deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse\n
-              deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse\n
-              deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse\n\n/' /etc/apt/sources.list
+              sed -i '1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse\n\n' /etc/apt/sources.list
               sudo apt-get update
               sudo apt-get install docker
               sudo service docker start
