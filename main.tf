bb20db6f568f722f939a80999548934540bb350f
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-16 12:04:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,9 +40,10 @@ resource "aws_instance" "web" {
               sudo ufw default allow outgoing
               echo "breakpoint2"
               #echo "" >> 
-              #sudo sed -i '1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse\n\n' /etc/apt/sources.list
-              sudo apt-get update
+              sudo sed -i '1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse\ndeb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse\n\n' /etc/apt/sources.list
               echo "breakpoint3"
+              sudo apt-get update
+              echo "breakpoint4"
               sudo apt-get install docker
               sudo service docker start
               sudo usermod -a -G docker ec2-user
