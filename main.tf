552e7dd8b46e27529fc81f0573f1e95a70406699
sciortinogiulio0 <63776700+sciortinogiulio0@users.noreply.github.com>
2022-01-17 17:03:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,14 +38,16 @@ resource "aws_instance" "web" {
               #!/bin/bash
               sudo apt-get update -y
               sudo apt-get upgrade -y
-              sudo apt-get install docker -y
+              sudo apt-get install docker.io -y
               sudo apt-get install awscli -y
               echo "breakpoint: docker and awscli installed"
               aws --version
               sudo service docker start -y
-              echo $'aws_access_key_id = ${var.aws_access_key_id}\naws_secret_access_key = ${var.aws_secret_access_key}' >> ~/.aws/credentials' >> ~/.aws/config
-              echo $'[default]\nregion = us-west-2' >> ~/.aws/config
-              cat ~/.aws/credentials
+              export AWS_ACCESS_KEY_ID=${var.aws_access_key_id}
+              export AWS_SECRET_ACCESS_KEY=${var.aws_secret_access_key}
+              #echo $'aws_access_key_id = ${var.aws_access_key_id}\naws_secret_access_key = ${var.aws_secret_access_key}' >> ~/.aws/credentials' >> ~/.aws/config
+              #echo $'[default]\nregion = us-west-2' >> ~/.aws/config
+              #cat ~/.aws/credentials
               sudo docker login -u AWS -p $(aws ecr get-login-password --region us-west-2) 099178467731.dkr.ecr.us-west-2.amazonaws.com
               sudo docker pull 099178467731.dkr.ecr.us-west-2.amazonaws.com/my-app:4
               EOF
